0615f4a483defa03abd5e18782593def88a49cf9
Anshul <anshulraturi007@gmail.com>
2023-10-29 15:40:46 +0530

    Products from Firestore

--- src/LogIn.jsx
+++ src/LogIn.jsx
@@ -73,57 +73,57 @@ const LogIn = ({ setLoggedIn }) => {
     }
   };
   return (
-    <div className="LoginPage flex flex-col items-center justify-center">
-      <h1 className="text-center text-5xl mt-20 mb-20 md:m-20">Log In</h1>
+    <div className='LoginPage flex flex-col items-center justify-center'>
+      <h1 className='text-center text-5xl mt-20 mb-20 md:m-20'>Log In</h1>
       <form
-        autoComplete="off"
-        className="flex flex-col items-center  bg-slate-100 w-96 md:w-1/2 lg:w-1/3 justify-center"
+        autoComplete='off'
+        className='flex flex-col items-center  bg-slate-100 w-96 md:w-1/2 lg:w-1/3 justify-center'
       >
-        <div className="name">
-          <label htmlFor="Name">User Name :</label>
+        <div className='name'>
+          <label htmlFor='Name'>User Name :</label>
           <input
-            autoComplete="off"
-            className="my-10 ml-5"
-            type="text"
-            id="Name"
-            name="name"
-            placeholder="Enter Your Name"
+            autoComplete='off'
+            className='my-10 ml-5'
+            type='text'
+            id='Name'
+            name='name'
+            placeholder='Enter Your Name'
             value={loginInfo.name}
             onChange={formChangeHandler}
           />
         </div>
-        <div className="email">
-          <label htmlFor="Email">Enter Your Email :</label>
+        <div className='email'>
+          <label htmlFor='Email'>Enter Your Email :</label>
           <input
-            autoComplete="off"
-            className="my-10 ml-5"
-            type="text"
-            id="Email"
-            name="email"
+            autoComplete='off'
+            className='my-10 ml-5'
+            type='text'
+            id='Email'
+            name='email'
             value={loginInfo.email}
-            placeholder="Enter Your Email"
+            placeholder='Enter Your Email'
             onChange={formChangeHandler}
           />
         </div>
-        <div className="password">
-          <label htmlFor="Password">Enter Your Password :</label>
+        <div className='password'>
+          <label htmlFor='Password'>Enter Your Password :</label>
           <input
-            className="my-10 ml-5"
-            type="password"
-            id="Password"
+            className='my-10 ml-5'
+            type='password'
+            id='Password'
             value={loginInfo.password}
-            name="password"
-            placeholder="Enter Your Password"
+            name='password'
+            placeholder='Enter Your Password'
             onChange={formChangeHandler}
           />
         </div>
         <button
-          className="bg-slate-200 hover:bg-slate-300 w-28 h-10 rounded-md"
+          className='bg-slate-200 hover:bg-slate-300 w-28 h-10 rounded-md'
           onClick={logInHandler}
         >
           Log In
         </button>
-        <p className="text-red-600">{errorMsg}</p>
+        <p className='text-red-600'>{errorMsg}</p>
       </form>
     </div>
   );
