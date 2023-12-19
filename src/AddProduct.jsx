2286ae664684b0176c80e7137c1b2598a2bfafae
Anshul <anshulraturi007@gmail.com>
2023-12-19 21:13:22 +0530

    Added form submission logic for add product page

--- src/AddProduct.jsx
+++ src/AddProduct.jsx
@@ -10,14 +10,22 @@ const AddProduct = ({ id }) => {
     category: "",
   });
 
-  // const productSubmitHandler = () => {
-
-  // };
+  // Function to handle changes in input fields
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProductInfo((prevProductInfo) => ({
+      ...prevProductInfo,
+      [name]: value,
+    }));
+  };
+  const productSubmitHandler = () => {
+    console.log(productInfo);
+  };
 
   return (
     <div className='flex flex-col items-center'>
       <h1 className='text-4xl'>Add Product</h1>
-      <div className='flex md:flex-row flex-col w-full h-screen md:justify-around'>
+      <div className='flex md:flex-row flex-col w-full h-full md:justify-around'>
         <div className='flex flex-col items-center'>
           <label htmlFor='title' className='md:text-xl md:mt-4 text-lg'>
             Add Title Of Product
@@ -25,54 +33,92 @@ const AddProduct = ({ id }) => {
           <input
             type='text'
             className='bg-slate-100 border-2 border-slate-200'
+            onChange={handleInputChange}
+            name='title'
           />
-          <label htmlFor='title' className='md:text-xl md:mt-4 text-lg'>
+          <label htmlFor='description' className='md:text-xl md:mt-4 text-lg'>
             Add Description Of Product
           </label>
           <textarea
             cols={35}
-            rows={35}
+            rows={10}
             placeholder='Enter Description...'
             className='bg-slate-100 border-2 border-slate-200'
+            onChange={handleInputChange}
+            name='description'
           />
         </div>
         <div className='flex flex-col items-center'>
-          <label htmlFor='title' className='md:text-xl md:mt-4 text-lg'>
-            Add Image Url Of Prduct
+          <label htmlFor='image' className='md:text-xl md:mt-4 text-lg'>
+            Add Image Url Of Product
           </label>
           <input
             type='text'
             className='bg-slate-100 border-2 border-slate-200'
+            onChange={handleInputChange}
+            name='image'
           />
-          <label htmlFor='title' className='md:text-xl md:mt-4 text-lg'>
+          <label htmlFor='price' className='md:text-xl md:mt-4 text-lg'>
             Add Product Price
           </label>
           <input
             type='text'
             className='bg-slate-100 border-2 border-slate-200'
+            onChange={handleInputChange}
+            name='price'
           />
-          <label htmlFor='title' className='md:text-xl md:mt-4 text-lg'>
+          <label htmlFor='category' className='md:text-xl md:mt-4 text-lg'>
             Add Product Category
           </label>
-          <radioGroup>
+          <div>
             <label className='m-2'>Laptops</label>
-            <input type='radio' name='category' value='men' />
+            <input
+              type='radio'
+              name='category'
+              value='Laptops'
+              onChange={handleInputChange}
+            />
             <br />
             <label className='m-2'>Electronics</label>
-            <input type='radio' name='category' value='men' />
+            <input
+              type='radio'
+              name='category'
+              value='Electronics'
+              onChange={handleInputChange}
+            />
             <br />
             <label className='m-2'>Clothing</label>
-            <input type='radio' name='category' value='men' />
+            <input
+              type='radio'
+              name='category'
+              value='Clothing'
+              onChange={handleInputChange}
+            />
             <br />
             <label className='m-2'>Sports</label>
-            <input type='radio' name='category' value='men' />
+            <input
+              type='radio'
+              name='category'
+              value='Sports'
+              onChange={handleInputChange}
+            />
             <br />
             <label className='m-2'>Mobile Phone</label>
-            <input type='radio' name='category' value='men' />
+            <input
+              type='radio'
+              name='category'
+              value='Mobile Phone'
+              onChange={handleInputChange}
+            />
             <br />
             <label className='m-2'>Jewellery</label>
-            <input type='radio' name='category' value='men' />
-          </radioGroup>
+            <input
+              type='radio'
+              name='category'
+              value='Jewellery'
+              onChange={handleInputChange}
+            />
+          </div>
         </div>
       </div>
       <button
